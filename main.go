c20cf0e160c5cdd43829398731cc0dc8058848fe
Darren Chan <darren@darchan0319@gmail.com>
2020-04-08 20:55:59 -0700

    Setup logging to output to ./log/info.log

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	// "api"
-	"fmt"
-	"github.com/joho/godotenv"
-	"log"
+	// "alpaca"
+
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
+	log "github.com/sirupsen/logrus"
 )
 
 func loadEnv() {
@@ -18,12 +20,43 @@ func loadEnv() {
 	godotenv.Load(".env." + env)
 }
 
+// Sets the logging configurations
+func setLogConfigs() *os.File {
+	log.SetLevel(log.WarnLevel)
+	if os.Getenv("BVB_ENV") == "dev" || os.Getenv("BVB_ENV") == "" {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	file, err := os.OpenFile("./log/info.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0644)
+	if os.Getenv("BVB_ENV") != "dev" {
+		// TODO: Send to a prod/stg cloud log file.
+	}
+
+	if err != nil {
+		log.Warn(err)
+	}
+
+	log.SetOutput(file)
+	log.SetFormatter(&log.JSONFormatter{})
+
+	return file
+}
+
 func main() {
 	loadEnv()
+	file := setLogConfigs()
+	defer file.Close()
+
 	port := os.Getenv("PORT")
-	fmt.Printf("listening on port: %s...", port)
+	log.Info("listening on port: %s...", port)
 
 	serverMux := http.NewServeMux()
+
 	// api.initialize(serverMux)
-	log.Fatal(http.ListenAndServe(":"+port, serverMux))
+	log.Fatal(http.ListenAndServeTLS(
+		":"+port,
+		"https-server.crt",
+		"https-server.key",
+		serverMux,
+	))
 }
